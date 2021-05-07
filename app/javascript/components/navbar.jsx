1ed3cfa173964230e10eb4836df11c67d39180d1
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-05-07 10:40:39 +0200

    logo image

--- app/javascript/components/navbar.jsx
+++ app/javascript/components/navbar.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { Link as LinkR } from 'react-router-dom'
 import { Link as LinkS } from 'react-scroll'
 import {FaBars} from 'react-icons/fa'
-
+import logoimage from '../images/logo.png'
 const Navbar = () => {
     return (
         <nav className="navbar-pict">
             <div className="navbar-container">
+                <LinkR className="navlogo">
+                    <img src={logoimage}></img>
+                </LinkR>
                 <ul>
                     <li>
                     <LinkR to="#">blablabla</LinkR>
