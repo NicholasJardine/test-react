fbaf047f077f4d3deb05f6c83206a8f3aeb3d1e5
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-05-10 11:15:33 +0200

    begin hero section

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './navbar'
 import Footer from './footer'
+import Hero from './hero'
 const App = () => {
     return (
         <>
         <Navbar/>
+        <Hero/>
         <Footer/>
         </>
     )
---
+++ app/javascript/components/hero.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+import heropic from '../images/cinema.jpg'
+
+const Hero = () => {
+
+    // const [isOpen, setIsOpen] = useState(false)
+    // const toggle = () => {
+    //   setIsOpen(!isOpen)
+    // }
+    return (
+        <div className="hero-container">
+            <div className="hero-content">
+
+                <div className="hero-items">
+                <h1 className="hero-h1">Pictor</h1>
+                    <p className="hero-text">The best platform for online auditions for actors and casting agents/production companies.</p>
+                    {/* <HeroBtn><p>Place Order</p></HeroBtn> */}
+                    {/* <HeroButton to='about' smooth={true} duration={500} spy={true} exact='true' offset= {-80}> Learn More</HeroButton> */}
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Hero
+
+
