453c83a113abae2c4e7ec0d663877745c28687b2
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-05-10 14:51:54 +0200

    corrections on styling on image

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Navbar from './navbar'
 import Footer from './footer'
 import Hero from './hero'
+import InfoSection from './infoact'
 const App = () => {
     return (
         <>
         <Navbar/>
         <Hero/>
+        <InfoSection/>
         <Footer/>
         </>
     )
