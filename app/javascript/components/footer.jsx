76ffbac50259abd4612470314cf7eb67f2d61d34
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-05-10 16:43:32 +0200

    footer links accurately labeled

--- app/javascript/components/footer.jsx
+++ app/javascript/components/footer.jsx
@@ -12,23 +12,35 @@ const Footer = () => {
                     <div className="footer-links-wrapper">
                         <div className="footer-links-item">
                             <h1 className='footer-links-title'>About Us</h1>
+                            <Link to="#" className="footer-link"> How it works</Link>
                             <Link to="#" className="footer-link"> Testimonials</Link>
                             <Link to="#" className="footer-link"> Investors</Link>
                             <Link to="#" className="footer-link"> Terms of Service</Link>
                         </div>
 
                         <div className="footer-links-item">
-                            <h1 className="footer-links-title">About Us</h1>
-                            <Link to="#" className="footer-link"> Testimonials</Link>
-                            <Link to="#" className="footer-link"> Investors</Link>
-                            <Link to="#" className="footer-link"> Terms of Service</Link>
+                            <h1 className="footer-links-title">Videos</h1>
+                            <Link to="#" className="footer-link"> Submit Video</Link>
+                            <Link to="#" className="footer-link"> Ambassadors</Link>
+                            <Link to="#" className="footer-link"> Agency</Link>
+                            <Link to="#" className="footer-link"> Influencer</Link>
                         </div>
 
                         <div className="footer-links-item">
-                            <h1 className="footer-links-title">About Us</h1>
-                            <Link to="#" className="footer-link"> Testimonials</Link>
-                            <Link to="#" className="footer-link"> Investors</Link>
-                            <Link to="#" className="footer-link"> Terms of Service</Link>
+                            <h1 className="footer-links-title">Contact Us</h1>
+                            <Link to="#" className="footer-link"> Contact</Link>
+                            <Link to="#" className="footer-link"> Support</Link>
+                            <Link to="#" className="footer-link"> FAQ</Link>
+                            <Link to="#" className="footer-link"> Sponsorships</Link>
+                        </div>
+
+
+                        <div className="footer-links-item">
+                            <h1 className="footer-links-title">Social Media</h1>
+                            <Link to="#" className="footer-link"> Instagram</Link>
+                            <Link to="#" className="footer-link"> Facebook</Link>
+                            <Link to="#" className="footer-link"> YouTube</Link>
+                            <Link to="#" className="footer-link"> Twitter</Link>
                         </div>
 
                     </div>
