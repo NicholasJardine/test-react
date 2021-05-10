5a8404680af15c6f6fe6a2d90429ac58e51dc958
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-05-10 15:22:35 +0200

    company info section getting there

--- app/javascript/components/app.jsx
+++ app/javascript/components/app.jsx
@@ -3,12 +3,14 @@ import Navbar from './navbar'
 import Footer from './footer'
 import Hero from './hero'
 import InfoSection from './infoact'
+import InfoComp from './infocomp'
 const App = () => {
     return (
         <>
         <Navbar/>
         <Hero/>
         <InfoSection/>
+        <InfoComp/>
         <Footer/>
         </>
     )
---
+++ app/javascript/components/infocomp.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+// import Button from '../ButtonElement'
+
+import actor from '../images/company.svg'
+
+const InfoComp = () => {
+
+    return (
+        <>
+        <div className="info-container-2" id="discover">
+            <div className="info-wrapper-2">
+                <div className="info-row-2">
+                    <div className="column-1">
+                        <div className="text-wrapper">
+                            <div className="topline">Find the perfect fit</div>
+                            <h1 className="heading-2">Looking for Actors?</h1>
+                            <p className="subtitle-2">Actors can sign up to the website and become a part of a database of actors, with the ability to add their bios, agents or contact information, websites and pictures/videos to their profiles. They will also receive briefs that match their profile and upload their audition tapes and send it directly to the casting agents.</p>
+                        </div>
+                    </div>
+                    <div className="column-2">
+                        <div className="img-wrap">
+                            <img className="image-style" src={actor} alt=""/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </>
+    )
+}
+
+export default InfoComp
