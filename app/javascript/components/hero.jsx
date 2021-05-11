451e979f1ccaefc1a8cd03c30d3e7cb094990d0e
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-05-11 12:54:08 +0200

    Learn more button working on index

--- app/javascript/components/hero.jsx
+++ app/javascript/components/hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {Link} from 'react-scroll'
 import heropic from '../images/cinema.jpg'
 
 const Hero = () => {
@@ -18,7 +18,7 @@ const Hero = () => {
                 <div className="hero-items">
                 <h1 className="hero-h1">Pictor</h1>
                     <p className="hero-text">The best platform for online auditions for actors and casting agents/production companies.</p>
-                    <Link to="#" className="hero-button">Learn More</Link>
+                    <Link to="about" smooth={true} duration={500} spy={true} exact='true' offset= {-80} className="hero-button">Learn More</Link>
                 </div>
             </div>
         </div>
