70fa9366638b8ea657f8b8bf490d6be8905e61cd
Nicholas Jardine <nicholasjardine0@gmail.com>
2021-05-12 15:50:54 +0200

    sidebar working mostly

--- app/javascript/components/sidebar.jsx
+++ app/javascript/components/sidebar.jsx
@@ -4,115 +4,29 @@ import {Link} from 'react-scroll';
 import {Link as LinkR} from 'react-router-dom';
 
 
-
-
-
-// const Sidebar = ({isOpen, toggle}) => {
-//     return (
-// <aside className="sidebar-container" isOpen={isOpen} toggle={toggle} >
-//     <div className="icon" onClick={toggle}>
-//         <FaTimes className="close-icon"/>
-//     </div>
-//     <div className="sidebar-wrapper">
-//         <ul className="sidebar-menu">
-//             <Link className="sidebar-link">
-//                 About
-//             </Link>
-//             <Link className="sidebar-link">
-//                 About
-//             </Link>
-
-//             <Link className="sidebar-link">
-//                 About
-//             </Link> 
-//             <Link className="sidebar-link">
-//                 About
-//             </Link>
-//                    </ul>
-//         <div className="sidebtn-wrap">
-//             <LinkR className="sidebar-route"></LinkR>
-//         </div>
-//     </div>
-// </aside>
-
-//     )
-// }
-
-// export default Sidebar
-
-
-
-// const Sidebar = ({toggle}) => {
-//     return (
-// <aside className={this.state.clicked ? 'clicked-sidebar' : 'unclicked-sidebar'} onClick={toggle}  >
-//     <div className="icon" onClick={toggle}>
-//         <FaTimes className="close-icon"/>
-//     </div>
-//     <div className="sidebar-wrapper">
-//         <ul className="sidebar-menu">
-//             <Link className="sidebar-link">
-//                 About
-//             </Link>
-//             <Link className="sidebar-link">
-//                 About
-//             </Link>
-
-//             <Link className="sidebar-link">
-//                 About
-//             </Link> 
-//             <Link className="sidebar-link">
-//                 About
-//             </Link>
-//                    </ul>
-//         <div className="sidebtn-wrap">
-//             <LinkR className="sidebar-route"></LinkR>
-//         </div>
-//     </div>
-// </aside>
-
-//     )
-// }
-
-// export default Sidebar
-
-
-
 const Sidebar = ({isOpen, toggle}) => {
-    // constructor(props){
-    //     super(props);
-    //     this.state={
-    //       clicked: false  
-    //     };
-    // }
 
-    // handleClick = () => {
-    //     this.setState({
-    //         clicked: !this.state.clicked
-    //     });
-    // }
         return(
-                <aside isOpen={isOpen} className={isOpen ? 'clicked-sidebar' : 'unclicked-sidebar'}  onClick={toggle} >
+                <aside className={isOpen ? 'clicked-sidebar' : 'unclicked-sidebar'}  onClick={toggle} >
                     <div className="icon" onClick={toggle}>
                         <FaTimes className="close-icon"/>
                     </div>
                     <div className="sidebar-wrapper">
                         <ul className="sidebar-menu">
-                            <Link className="sidebar-link">
-                                About
-                            </Link>
-                            <Link className="sidebar-link">
-                                About
-                            </Link>
-
-                            <Link className="sidebar-link">
-                                About
-                            </Link> 
+            
+                    <Link className="sidebar-link" to='about' smooth={true} duration={500} spy={true} exact='true' offset= {-80}>About</Link>
+                 
+                    <Link className="sidebar-link" to='discover' smooth={true} duration={500} spy={true} exact='true' offset= {-80}>Discover</Link>
+                    <Link className="sidebar-link" to='discover' smooth={true} duration={500} spy={true} exact='true' offset= {-80}>News</Link>
+                            <a className="sidebar-link" herf="users/sign_up">
+                                Sign Up
+                            </a> 
                             <Link className="sidebar-link">
                                 About
                             </Link>
                                 </ul>
                         <div className="sidebtn-wrap">
-                            <LinkR className="sidebar-route"></LinkR>
+                            <a href="users/sign_in" className="sidebar-route">Sign in</a>
                         </div>
                     </div>
                 </aside>
